ac087e2ab46e4a9532b794cce342b1bcc6f145ba
Jaime Chavarriaga <jaime.chavarriaga@gmail.com>
2024-05-04 01:11:03 +0000

    Agrega pruebas para Agregar Ubicación a Trayecto

--- trayectos/src/test/java/com/bicicletas/trayectos/logica/AgregarUbicacionATrayectoTest.java
+++ trayectos/src/test/java/com/bicicletas/trayectos/logica/AgregarUbicacionATrayectoTest.java
@@ -3,6 +3,9 @@ package com.bicicletas.trayectos.logica;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,19 +29,29 @@ public class AgregarUbicacionATrayectoTest {
     @Autowired
     UbicacionesRepository ubicaciones;
 
+    @BeforeEach
+    void borraBaseDatos() {
+        ubicaciones.deleteAll();
+        trayectos.deleteAll();
+    }
+
     @Test
     @Transactional
     void grabarSinError() {
 
-        // Arrange -- preparar
-
-        Trayecto trayecto = new Trayecto();
-        trayecto.setEnProceso(true);
-        trayecto = trayectos.save(trayecto);
-
         try {
 
+            // Arrange -- preparar
+
+            // agrega un trayecto activo (sin finalizar)
+            Trayecto trayecto = new Trayecto();
+            trayecto.setEnProceso(true);
+            trayecto = trayectos.save(trayecto);
+        
+
             // Act -- actuar
+
+            // agrega una ubicación a un trayecto activo
             servicio.agregarUbicacionATrayecto(
                 trayecto.getId(), 
                 27.0, 
@@ -46,6 +59,7 @@ public class AgregarUbicacionATrayectoTest {
     
             // Assert -- revisar el estado
 
+            // revisa los datos en los objetos de la base de datos
             trayecto = trayectos.findById(trayecto.getId()).get();
             Ubicacion nuevaUbicacion = trayecto.getUbicaciones().get(0);
             
@@ -61,10 +75,57 @@ public class AgregarUbicacionATrayectoTest {
     @Test
     void grabarEnUnTrayectoQueNoExiste() {
 
+        try {
+
+            // Arrange -- preparar        
+
+            // Act -- actuar
+
+            // agrega una ubicación a un trayecto que no existe
+            UUID id = UUID.randomUUID();
+            servicio.agregarUbicacionATrayecto(
+                id, 
+                27.0, 
+                42.0);
+    
+            // Assert -- revisar el estado
+
+            fail("Debio generar excepción");
+
+        } catch (Exception e) {
+            // ok
+        }
+
     }
 
     @Test
     void grabarEnUnTrayectoYaFinalizado() {
+
+        try {
+
+            // Arrange -- preparar
+
+            // crea un trayecto finalizado
+            Trayecto trayecto = new Trayecto();
+            trayecto.setEnProceso(false);
+            trayecto = trayectos.save(trayecto);
+
+
+            // Act -- actuar
+
+            // agrega una ubicación al trayecto finalizado
+            servicio.agregarUbicacionATrayecto(
+                trayecto.getId(), 
+                27.0, 
+                42.0);
+    
+            // Assert -- revisar el estado
+
+            fail("debio generar excepción");
+
+        } catch (Exception e) {
+            // OK
+        }
 
     }
 
