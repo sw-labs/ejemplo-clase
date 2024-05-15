384bc23089531fcf9ed175a426a0ee761bb4836f
Jaime Chavarriaga <jaime.chavarriaga@gmail.com>
2024-05-15 10:36:38 +0000

    Elimina imports que no son necesarios

--- trayectos/src/main/java/com/bicicletas/trayectos/rest/TrayectosRestController.java
+++ trayectos/src/main/java/com/bicicletas/trayectos/rest/TrayectosRestController.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
